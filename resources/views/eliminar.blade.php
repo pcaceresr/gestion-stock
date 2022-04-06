cd25c3241c94dc8c9561cf2a89355e12daf041ee
Paula Caceres <paula.caceres.r@gmail.com>
2022-04-06 17:38:45 -0400

    se crea agregar, actualizar, eliminar y consultar

---
+++ resources/views/eliminar.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.master')
+
+@section('content')
+<div>
+    <center>
+        <br>
+        <h2>Eliminar producto</h2>
+        <br>
+    <form action="/action_page.php">
+        <div class="mb-3">
+            &nbsp &nbsp<label for="" class="form-label">Codigo del producto:
+            <input type="text" class="form-control" id="codigo"  name="codigo">
+        </div>
+        <div class="mb-3">
+            &nbsp &nbsp<label for="" class="form-label">Nombre del producto:
+            <input type="text" class="form-control" id="nombre"  name="nombre">
+        </div>
+        <div class="mb-3">
+            &nbsp &nbsp<label for="" class="form-label">Cantidad:
+            <input type="number" class="form-control" id="cantidad"  name="cantidad">
+        </div>
+   <br>
+        <button type="submit" class="btn btn-primary">Eliminar</button>
+        <button type="submit" class="btn btn-primary">Volver al menú principal </button>
+    </center>
+    </form>
+<//div>
+@stop
