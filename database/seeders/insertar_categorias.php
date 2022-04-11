a231681704d344bce41bba8c61c2fc365bbe2f54
Guillermo <guillermo.alegria.pizarro@ciisa.cl>
2022-04-10 22:48:49 -0400

    modificacion 01

---
+++ database/seeders/insertar_categorias.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class insertar_categorias extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        for ($i=1; $i < 10 ; $i++) { 
+            DB::table('categorias')->insert(array(
+                'name' => 'categoria'.$i,
+                
+            ));# code...
+        }
+        $this->command->info('Categoria insertada con exito!');
+    }
+}
