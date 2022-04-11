e0a38291c8bfadabfd5614cb2f87fe854f213ee1
Paula Caceres <paula.caceres.r@gmail.com>
2022-04-11 15:45:49 -0400

    generacion de modelos

--- app/Models/productoSucursal.php
+++ app/Models/productoSucursal.php
@@ -5,21 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class producto extends Model
+class productoSucursal extends Model
 {
-    protected $table = 'productos';
-    protected $primaryKey = 'id';
-
-    public function categoria()
-    {
-        return $this->belongsTo(categoria::class);
-    }
-
+  
     public function producto()
     {
         return $this->belongsToMany(productoSucursal::class);
     }
-        
-    use HasFactory;
+ 
 
+    use HasFactory;
 }
--- database/seeders/insertar_producto.php
+++ database/seeders/insertar_producto.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 
 class insertar_producto extends Seeder
 {
+    
     /**
      * Run the database seeds.
      *
@@ -15,12 +16,26 @@ class insertar_producto extends Seeder
     public function run()
     {
         //
-        for ($i=1; $i < 10 ; $i++) { 
+        for ($i=1; $i <= 10 ; $i++) { 
+             
             DB::table('productos')->insert(array(
                 'name' => 'producto'.$i,
+                'codigo' => 'codigo'.$i,
+                'cantidad' => $this->obtenerCantidad (),
+                'categoria_id' => $this->obtenerCategoria (),
                 
             ));# code...
         }
         $this->command->info('Productos insertados con exito!');
     }
+    
+    public function obtenerCategoria(){
+        return rand(1,10);
+    }
+
+    public function obtenerCantidad(){
+        return rand(1,100);
+    }
+
 }
+
